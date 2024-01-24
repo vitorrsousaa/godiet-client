05b6afc7f525e9e604ca4787acccb0af1a5b3532
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-24 00:01:44 -0300

    feat: created lazy custom

---
+++ src/app/utils/lazyLoad.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable indent */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Spinner } from '@godiet-components/Spinner';
+
+import loadable from '@loadable/component';
+
+export const lazyLoad = <T extends Record<string, any>, U extends keyof T>(
+  loader: (x?: string) => Promise<T>
+) =>
+  new Proxy({} as unknown as T, {
+    get: (_, componentName: string | symbol) => {
+      if (typeof componentName === 'string') {
+        return loadable(
+          () =>
+            Promise.all([
+              loader(componentName),
+              new Promise((resolve) => setTimeout(resolve, 200)),
+            ]).then(([module]) => ({
+              default: module[
+                componentName as U
+              ] as any as React.ComponentType<any>,
+            })),
+          {
+            fallback: (
+              <div className="grid h-full w-full place-items-center bg-transparent">
+                <Spinner className="fill-gray-200 text-teal-700" />
+              </div>
+            ),
+            cacheKey: () => componentName.toString(),
+          }
+        );
+      }
+    },
+  });
--- src/view/ui/Router/Router.tsx
+++ src/view/ui/Router/Router.tsx
@@ -1,52 +1,83 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Suspense } from 'react';
+
+import { Logo } from '@godiet-components/Logo';
+import { Spinner } from '@godiet-components/Spinner';
 import { routes } from '@godiet-config';
-import { DashboardLayout } from '@godiet-layouts/DashboardLayout';
-import { SettingsLayout } from '@godiet-layouts/SettingsLayout';
-import { Dashboard } from '@godiet-pages/Dashboard';
-import { Login } from '@godiet-pages/Login';
-import { Patients } from '@godiet-pages/Patients';
-import { Register } from '@godiet-pages/Register';
-import { Settings } from '@godiet-pages/Settings';
-import { SettingsNotification } from '@godiet-pages/SettingsNotification';
-import { SettingsRewards } from '@godiet-pages/SettingsRewards';
+import { NotFound } from '@godiet-pages/NotFound';
+import { Patient } from '@godiet-pages/Patient';
+import { lazyLoad } from '@godiet-utils/lazyLoad';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { AuthGuard } from './AuthGuard';
 
+const { DashboardLayout } = lazyLoad(
+  () => import('@godiet-layouts/DashboardLayout')
+);
+const { SettingsLayout } = lazyLoad(
+  () => import('@godiet-layouts/SettingsLayout')
+);
+const { Dashboard } = lazyLoad(() => import('@godiet-pages/Dashboard'));
+const { Login } = lazyLoad(() => import('@godiet-pages/Login'));
+const { Register } = lazyLoad(() => import('@godiet-pages/Register'));
+const { Settings } = lazyLoad(() => import('@godiet-pages/Settings'));
+const { SettingsNotification } = lazyLoad(
+  () => import('@godiet-pages/SettingsNotification')
+);
+const { SettingsRewards } = lazyLoad(
+  () => import('@godiet-pages/SettingsRewards')
+);
+const { Patients } = lazyLoad(() => import('@godiet-pages/Patients'));
+
 export function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path={routes.LOGIN} element={<Login />} />
-          <Route path={routes.REGISTER} element={<Register />} />
-        </Route>
-        <Route element={<AuthGuard isPrivate={true} />}>
-          <Route element={<DashboardLayout />}>
-            <Route path={routes.HOME} element={<Dashboard />} />
-            <Route element={<SettingsLayout />}>
-              <Route path={routes.SETTINGS} element={<Settings />} />
-              <Route
-                path={routes.SETTINGS_NOTIFICATIONS}
-                element={<SettingsNotification />}
-              />
-              <Route
-                path={routes.SETTINGS_REWARDS}
-                element={<SettingsRewards />}
-              />
-              <Route
-                path={routes.SETTINGS_PAYMENTS}
-                element={<div>Pagamentos</div>}
-              />
-              <Route
-                path={routes.SETTINGS_MYPLAN}
-                element={<div>Meu plano</div>}
-              />
-            </Route>
-            <Route path={routes.PATIENTS} element={<Patients />} />
+    <Suspense
+      fallback={
+        <div className="fixed left-0 top-0 grid h-full w-full place-items-center bg-teal-900">
+          <div className="flex flex-col items-center gap-4">
+            <Logo className="text-3xl text-white" />
+
+            <Spinner className="fill-white text-teal-700" />
+          </div>
+        </div>
+      }
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path={'*'} element={<NotFound />} />
+          <Route element={<AuthGuard isPrivate={false} />}>
+            <Route path={routes.LOGIN} element={<Login />} />
+            <Route path={routes.REGISTER} element={<Register />} />
           </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route element={<AuthGuard isPrivate={true} />}>
+            <Route element={<DashboardLayout />}>
+              <Route path={routes.HOME} element={<Dashboard />} />
+              <Route element={<SettingsLayout />}>
+                <Route path={routes.SETTINGS} element={<Settings />} />
+                <Route
+                  path={routes.SETTINGS_NOTIFICATIONS}
+                  element={<SettingsNotification />}
+                />
+                <Route
+                  path={routes.SETTINGS_REWARDS}
+                  element={<SettingsRewards />}
+                />
+                <Route
+                  path={routes.SETTINGS_PAYMENTS}
+                  element={<div>Pagamentos</div>}
+                />
+                <Route
+                  path={routes.SETTINGS_MYPLAN}
+                  element={<div>Meu plano</div>}
+                />
+              </Route>
+              <Route path={routes.PATIENTS} element={<Patients />} />
+              <Route path={routes.PATIENTS_BY_ID} element={<Patient />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
