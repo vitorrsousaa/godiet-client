0315e123a5b402c45ab7fabab53a27cec94cdacc
Vitor Sousa <v.sousa.cf@gmail.com>
2024-02-11 20:17:13 -0300

    chore: increase package for tests

---
+++ vitest.config.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      '**/src/app/tests/**/index.ts',
+      '**/html/**',
+      '**/coverage/**',
+      '**/src/server/**',
+      '**/src/factories/**',
+    ],
+    reporters: 'html',
+    coverage: {
+      provider: 'v8',
+    },
+    globals: true,
+    alias: {
+      '@godiet-assets': path.resolve(__dirname, './src/view/assets'),
+      '@godiet-components': path.resolve(__dirname, './src/view/ui'),
+      '@godiet-hooks': path.resolve(__dirname, './src/app/hooks'),
+      '@godiet-storage': path.resolve(__dirname, './src/app/storage'),
+      '@godiet-utils': path.resolve(__dirname, './src/app/utils'),
+      '@godiet-pages': path.resolve(__dirname, './src/view/pages'),
+      '@godiet-layouts': path.resolve(__dirname, './src/view/layouts'),
+      '@godiet-contexts': path.resolve(__dirname, './src/app/contexts'),
+      '@godiet-config': path.resolve(__dirname, './src/app/config/index.ts'),
+      '@godiet-query': path.resolve(__dirname, './src/app/libs/query.tsx'),
+      '@godiet-services': path.resolve(__dirname, './src/app/services'),
+      '@godiet-entities': path.resolve(
+        __dirname,
+        './src/app/entities/index.ts'
+      ),
+    },
+  },
+});
