3fc88e93836c5f5de148638b2b1aee05f5acd752
Vitor Sousa <v.sousa.cf@gmail.com>
2024-02-17 19:51:21 -0300

    feat: implements microsoft clarity

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -4,8 +4,15 @@ import { Router } from '@godiet-ui/Router';
 import { ThemeProvider } from '@godiet-ui/ThemeProvider';
 
 import { Toaster } from 'react-hot-toast';
+import { clarity } from 'react-microsoft-clarity';
+
+const { DEV: IS_DEVELOPMENT } = import.meta.env;
 
 function App() {
+  if (!IS_DEVELOPMENT) {
+    clarity.init('l3dsncrb0t');
+  }
+
   return (
     <QueryClientProvider>
       <AuthProvider>
