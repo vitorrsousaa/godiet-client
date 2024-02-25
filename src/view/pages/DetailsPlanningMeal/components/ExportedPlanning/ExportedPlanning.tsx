ae7dcfd79c887bd22f40133805cd44bcf3ce7b3a
Vitor Sousa <v.sousa.cf@gmail.com>
2024-02-25 20:11:21 -0300

    fix: changes use name of patient

--- src/view/pages/DetailsPlanningMeal/components/ExportedPlanning/ExportedPlanning.tsx
+++ src/view/pages/DetailsPlanningMeal/components/ExportedPlanning/ExportedPlanning.tsx
@@ -2,6 +2,7 @@ import { forwardRef } from 'react';
 
 import { CardMeal } from '@godiet-components/CardMeal';
 import { TPlanningMealDetails } from '@godiet-entities';
+import { usePatient } from '@godiet-hooks/patient';
 
 interface ExportedPlanningProps {
   planningMeal: TPlanningMealDetails;
@@ -13,49 +14,55 @@ export const ExportedPlanning = forwardRef<
 >((props, ref) => {
   const { planningMeal } = props;
 
+  const { patient } = usePatient();
+
   return (
     <div ref={ref} className=" hidden flex-col gap-4 ">
-      <div className="ga-3 flex w-full flex-col items-center justify-center">
-        <strong className="text-2xl text-black">Jonas oliveira</strong>
-        <small className="text-xl dark:text-black">
-          Planejamento alimentar
-        </small>
-      </div>
-
-      {planningMeal && (
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-row items-center justify-between">
-            <h4>{planningMeal.name}</h4>
+      {patient && (
+        <>
+          <div className="ga-3 flex w-full flex-col items-center justify-center">
+            <strong className="text-2xl text-black">{patient.name}</strong>
+            <small className="text-xl dark:text-black">
+              Planejamento alimentar
+            </small>
           </div>
-          <div className="flex flex-col gap-4">
-            {planningMeal.meals.map((meal) => {
-              return (
-                <CardMeal.Root key={meal.id}>
-                  <CardMeal.Header description={meal.time}>
-                    {meal.name}
-                  </CardMeal.Header>
-                  <CardMeal.Content>
-                    {meal.foods.length > 0 && (
-                      <>
-                        <CardMeal.ListHeader className="dark:bg-teal-800" />
 
-                        {meal.foods
-                          .filter((food) => food.options.length > 0)
-                          .map((food) => (
-                            <CardMeal.Options
-                              foods={food}
-                              key={food.id}
-                              className="dark:bg-teal-600"
-                            />
-                          ))}
-                      </>
-                    )}
-                  </CardMeal.Content>
-                </CardMeal.Root>
-              );
-            })}
-          </div>
-        </div>
+          {planningMeal && (
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-row items-center justify-between">
+                <h4>{planningMeal.name}</h4>
+              </div>
+              <div className="flex flex-col gap-4">
+                {planningMeal.meals.map((meal) => {
+                  return (
+                    <CardMeal.Root key={meal.id}>
+                      <CardMeal.Header description={meal.time}>
+                        {meal.name}
+                      </CardMeal.Header>
+                      <CardMeal.Content>
+                        {meal.foods.length > 0 && (
+                          <>
+                            <CardMeal.ListHeader className="dark:bg-teal-800" />
+
+                            {meal.foods
+                              .filter((food) => food.options.length > 0)
+                              .map((food) => (
+                                <CardMeal.Options
+                                  foods={food}
+                                  key={food.id}
+                                  className="dark:bg-teal-600"
+                                />
+                              ))}
+                          </>
+                        )}
+                      </CardMeal.Content>
+                    </CardMeal.Root>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
