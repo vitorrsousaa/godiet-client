89a9927d65cc877f0f4dc75f0a6aa6fb799ff31b
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-07 17:01:57 -0300

    feat: include method to add food on meal

--- src/view/pages/CreatePlanning/components/modals/AddFoodModal/AddFoodModal.hook.ts
+++ src/view/pages/CreatePlanning/components/modals/AddFoodModal/AddFoodModal.hook.ts
@@ -1,22 +1,76 @@
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 
 import { useGetAllFoods } from '@godiet-hooks/foods';
+import { TCreatePlanningMealDTO } from '@godiet-pages/CreatePlanning/CreatePlanning.hook';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useFieldArray, useForm, useFormContext } from 'react-hook-form';
+import * as z from 'zod';
 
 import { AddFoodModalProps } from './AddFoodModal';
 
+const CreateMealFoodSchema = z.object({
+  id: z.string().uuid(),
+  measure: z.string().min(1),
+  qty: z.number().nonnegative().min(1),
+});
+
+type TCreateMealDTO = z.infer<typeof CreateMealFoodSchema>;
+
 export function useAddFoodModalHook(props: AddFoodModalProps) {
-  const { onClose } = props;
+  const { mealIndex, onClose } = props;
 
   const { foods, isFetchingFoods } = useGetAllFoods();
 
-  const handleSubmit = useCallback(() => {
-    console.log('data');
-  }, [onClose]);
+  // Internal Form
+  const {
+    handleSubmit: hookFormSubmit,
+    register,
+    formState: { errors, isValid: internalFormIsValid },
+    control: internalControl,
+  } = useForm<TCreateMealDTO>({
+    resolver: zodResolver(CreateMealFoodSchema),
+    defaultValues: {
+      id: '',
+      measure: '',
+      qty: 1,
+    },
+  });
+
+  // Internal Form
+
+  //External form
+  const { control } = useFormContext<TCreatePlanningMealDTO>();
+
+  const { append } = useFieldArray({
+    name: `meals.${mealIndex}.mealFoods`,
+    control,
+  });
+  //External form
+
+  const handleInternalFormSubmit = hookFormSubmit((data) => {
+    append({
+      name: 'comidinha',
+      measure: data.measure,
+      qty: data.qty,
+      id: data.id,
+    });
+
+    onClose();
+  });
 
   const foodOptions = useMemo(
     () => foods.map((food) => ({ label: food.name, value: food.id })),
     [foods]
   );
 
-  return { foodOptions, isFetchingFoods, handleSubmit };
+  return {
+    foodOptions,
+    isFetchingFoods,
+    errors,
+    internalControl,
+    internalFormIsValid,
+    register,
+    handleInternalFormSubmit,
+  };
 }
--- src/view/pages/CreatePlanning/components/modals/AddFoodModal/AddFoodModal.tsx
+++ src/view/pages/CreatePlanning/components/modals/AddFoodModal/AddFoodModal.tsx
@@ -1,20 +1,39 @@
+import React from 'react';
+
 import { Button } from '@godiet-ui/Button';
 import { Input } from '@godiet-ui/Input';
 import { Modal } from '@godiet-ui/Modal';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@godiet-ui/Select';
 import { SelectAutoComplete } from '@godiet-ui/SelectAutoComplete';
+import { Separator } from '@godiet-ui/Separator';
+
+import { Controller } from 'react-hook-form';
 
 import { useAddFoodModalHook } from './AddFoodModal.hook';
 
 export interface AddFoodModalProps {
   isOpen: boolean;
   onClose: () => void;
+  mealIndex: number;
 }
 
 export function AddFoodModal(props: AddFoodModalProps) {
   const { isOpen, onClose } = props;
 
-  const { foodOptions, isFetchingFoods, handleSubmit } =
-    useAddFoodModalHook(props);
+  const {
+    foodOptions,
+    isFetchingFoods,
+    internalControl,
+    internalFormIsValid,
+    handleInternalFormSubmit,
+  } = useAddFoodModalHook(props);
 
   // TODO- ADICIONAR ITENS QUANDO ESTIVER PESQUISANDO PELO ALIMENTO
   // - Medida caseira usual
@@ -22,7 +41,7 @@ export function AddFoodModal(props: AddFoodModalProps) {
   // - Referência (tabela TACO)
 
   return (
-    <Modal.Root isOpen={isOpen} onClose={onClose}>
+    <Modal.Root isOpen={isOpen} onClose={onClose} className="max-w-[740px]">
       <Modal.Header>
         <Modal.Title>Adicionando um novo alimento</Modal.Title>
         <Modal.Description>
@@ -30,18 +49,89 @@ export function AddFoodModal(props: AddFoodModalProps) {
         </Modal.Description>
       </Modal.Header>
 
-      <SelectAutoComplete
-        isLoading={isFetchingFoods}
-        placeholder="Selecione um alimento"
-        options={foodOptions}
-      />
-      <Input name="quantity" placeholder="Quantidade do alimento" />
+      <form
+        onSubmit={handleInternalFormSubmit}
+        id="form-meal-food"
+        className="flex flex-col gap-4"
+      >
+        <Controller
+          control={internalControl}
+          name="id"
+          render={({ field: { value, onChange } }) => (
+            <SelectAutoComplete
+              isLoading={isFetchingFoods}
+              placeholder="Selecione um alimento"
+              options={foodOptions}
+              value={value}
+              onChange={onChange}
+            />
+          )}
+        />
+        <div className="flex flex-col justify-between gap-2.5 sm:flex-row">
+          <span className="w-full">
+            <Controller
+              control={internalControl}
+              name="measure"
+              render={({ field: { value, onChange } }) => (
+                <Select value={value} onValueChange={onChange}>
+                  <SelectTrigger className="h-8  data-[placeholder]:text-muted-foreground sm:h-[3.25rem] ">
+                    <SelectValue placeholder="Selecione a unidade de medida" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="colher">Colher</SelectItem>
+                      <SelectItem value="colher2">Colher2</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              )}
+            />
+          </span>
+          <span className="w-full">
+            <Controller
+              control={internalControl}
+              name="qty"
+              render={({ field: { value, onChange, name } }) => (
+                <Input
+                  placeholder="Quantidade do alimento"
+                  type="number"
+                  value={value}
+                  onChange={(event) => {
+                    const newValue = event.target.value.replace(/^0+/, '');
+                    const numberValue = parseFloat(newValue);
+
+                    if (numberValue) {
+                      onChange(parseFloat(newValue));
+                    } else {
+                      onChange(0);
+                    }
+                  }}
+                  name={name}
+                />
+              )}
+            />
+          </span>
+        </div>
+      </form>
+
+      {internalFormIsValid && (
+        <>
+          <Separator />
+          <div>Adicionar informações sobre o alimento selecionado</div>
+        </>
+      )}
 
       <Modal.Footer>
         <Button onClick={onClose} variant={'destructive'}>
           Cancelar
         </Button>
-        <Button onClick={handleSubmit}>Adicionar</Button>
+        <Button
+          type="submit"
+          form="form-meal-food"
+          disabled={!internalFormIsValid}
+        >
+          Adicionar
+        </Button>
       </Modal.Footer>
     </Modal.Root>
   );
