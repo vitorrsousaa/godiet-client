54fcd09792d9c1693336b5763dfe1cf897c8a2d7
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-12 16:03:10 -0300

    refactor: move old planning meal and create new planning meal entities

--- src/app/services/planningMeal/getAllByPatient.ts
+++ src/app/services/planningMeal/getAllByPatient.ts
@@ -1,38 +1,5 @@
-import { TFood } from '@godiet-entities';
+import { TPlanningMeal } from '@godiet-entities';
 import { httpClient } from '@godiet-services/httpClient';
-
-interface TPlanningMeal {
-  id: string;
-  patientId: string;
-  date: string;
-  meals: string[];
-  name: string;
-  createdAt: string;
-}
-
-interface TPlanningMealDetails {
-  id: string;
-  patientId: string;
-  date: string;
-  meals: TMealsDetails[];
-  name: string;
-  createdAt: string;
-}
-
-interface TMealsDetails {
-  id: string;
-  name: string;
-  planningMealId: string;
-  time: string;
-  foods: TFoodDetails[];
-}
-
-interface TFoodDetails {
-  id: string;
-  categoryNameId: string;
-  portion: number;
-  options: TFood[];
-}
 
 interface getAllByPatientParams<TPlanning> {
   patientId: string;
@@ -40,7 +7,7 @@ interface getAllByPatientParams<TPlanning> {
 }
 
 type TPlanningMealResponse<TPlanning extends string | undefined> =
-  TPlanning extends string ? TPlanningMealDetails : TPlanningMeal[];
+  TPlanning extends string ? TPlanningMeal : TPlanningMeal[];
 
 export async function getAllByPatient<TPlanning extends string | undefined>(
   params: getAllByPatientParams<TPlanning>
