ca2db81f7d83fc62b2b809abe87cef61103e2479
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-17 19:45:11 -0300

    feat: create modal to create meal food

---
+++ src/view/pages/FavoritesMeals/components/modals/ModalCreateFavoriteMeal/ModalCreateFavoriteMeal.hook.ts
@@ -0,0 +1,185 @@
+import { useCallback, useMemo } from 'react';
+
+import { useCreateFavoriteMeal } from '@godiet-hooks/favoriteMeal';
+import { useGetAllFoods } from '@godiet-hooks/foods';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useFieldArray, useForm, useWatch } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import * as z from 'zod';
+
+import { ModalCreateFavoriteMealProps } from './ModalCreateFavoriteMeal';
+
+const CreateMealFoodSchema = z.object({
+  foodId: z.string().uuid(),
+
+  measure: z.string(),
+
+  qty: z.z.number().nonnegative().min(1),
+});
+
+const CreateFavoriteMealSchema = z.object({
+  name: z.string().min(1),
+
+  mealFood: z.array(CreateMealFoodSchema).min(1),
+});
+
+type TCreateMealFoodDTO = z.infer<typeof CreateMealFoodSchema>;
+
+type TCreateFavoriteMealDTO = z.infer<typeof CreateFavoriteMealSchema>;
+
+export function useModalCreateFavoriteMealHook(
+  props: ModalCreateFavoriteMealProps
+) {
+  const { onClose } = props;
+
+  const { foods, isFetchingFoods } = useGetAllFoods();
+
+  const { createFavoriteMeal, isCreatingFavoriteMeal } =
+    useCreateFavoriteMeal();
+
+  const methods = useForm<TCreateFavoriteMealDTO>({
+    resolver: zodResolver(CreateFavoriteMealSchema),
+
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const { control: internalControl, reset: resetInternalForm } =
+    useForm<TCreateMealFoodDTO>({
+      resolver: zodResolver(CreateMealFoodSchema),
+
+      defaultValues: {
+        foodId: '',
+
+        measure: '',
+
+        qty: 0,
+      },
+    });
+
+  const watchFood = useWatch({
+    control: internalControl,
+  });
+
+  const {
+    handleSubmit: hookFormSubmit,
+    formState: { isValid: formIsValid },
+    reset,
+    control,
+  } = methods;
+
+  const {
+    append: appendMeals,
+    remove: removeMeal,
+    fields: mealFoods,
+  } = useFieldArray({
+    control,
+    name: 'mealFood',
+  });
+
+  const foodOptions = useMemo(
+    () => foods.map((food) => ({ label: food.name, value: food.id })),
+    [foods]
+  );
+
+  const measureOptions = useMemo(() => {
+    if (!watchFood) return [];
+
+    if (!watchFood.foodId) return [];
+
+    const selectedFood = foods.find((food) => food.id === watchFood.foodId);
+
+    if (!selectedFood) return [];
+
+    return selectedFood.measures;
+  }, [foods, watchFood]);
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    const mapperMealFoods = data.mealFood.map((mealFoodUnit) => {
+      const selectedFood = foods.find(
+        (food) => food.id === mealFoodUnit.foodId
+      )!;
+
+      const selectedMeasure = selectedFood.measures.find(
+        (measure) => measure.name === mealFoodUnit.measure
+      );
+
+      return {
+        ...mealFoodUnit,
+        measure: selectedMeasure ?? selectedFood.measures[0],
+        options: [],
+      };
+    });
+
+    const newData = {
+      name: data.name,
+      mealFoods: mapperMealFoods,
+    };
+
+    try {
+      await createFavoriteMeal({
+        name: newData.name,
+        mealFoods: newData.mealFoods,
+      });
+
+      toast.success('Refeição criada com sucesso!');
+    } catch {
+      toast.error('Tivemos um erro ao criar');
+    } finally {
+      handleCloseModal();
+    }
+  });
+
+  const handleAppendMealFood = useCallback(() => {
+    const selectedFood = foods.find((food) => food.id === watchFood.foodId);
+
+    if (!selectedFood) {
+      return;
+    }
+
+    const selectedMeasure = selectedFood.measures.find(
+      (measure) => measure.name === watchFood.measure
+    );
+
+    if (!selectedMeasure) {
+      return;
+    }
+
+    if (!watchFood.qty) return;
+
+    if (watchFood.qty < 0) return;
+
+    appendMeals({
+      foodId: selectedFood.id,
+      measure: selectedMeasure.name,
+      qty: watchFood.qty,
+    });
+
+    resetInternalForm();
+  }, [appendMeals, foods, resetInternalForm, watchFood]);
+
+  const handleCloseModal = useCallback(() => {
+    onClose();
+    reset();
+    resetInternalForm();
+    removeMeal();
+  }, [onClose, removeMeal, reset, resetInternalForm]);
+
+  return {
+    handleCloseModal,
+    handleSubmit,
+    handleAppendMealFood,
+    removeMeal,
+    formIsValid,
+    foods,
+    isCreatingFavoriteMeal,
+    mealFoods,
+    control,
+    internalControl,
+    isFetchingFoods,
+    foodOptions,
+    measureOptions,
+  };
+}
---
+++ src/view/pages/FavoritesMeals/components/modals/ModalCreateFavoriteMeal/ModalCreateFavoriteMeal.tsx
@@ -0,0 +1,189 @@
+import { Button } from '@godiet-ui/Button';
+import { Input } from '@godiet-ui/Input';
+import { Modal } from '@godiet-ui/Modal';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@godiet-ui/Select';
+import { SelectAutoComplete } from '@godiet-ui/SelectAutoComplete';
+import { Separator } from '@godiet-ui/Separator';
+import { SimpleInput } from '@godiet-ui/SimpleInput';
+
+import { TrashIcon } from '@radix-ui/react-icons';
+import { Controller } from 'react-hook-form';
+
+import { useModalCreateFavoriteMealHook } from './ModalCreateFavoriteMeal.hook';
+
+export interface ModalCreateFavoriteMealProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function ModalCreateFavoriteMeal(props: ModalCreateFavoriteMealProps) {
+  const { isOpen } = props;
+
+  const {
+    handleCloseModal,
+    handleSubmit,
+    handleAppendMealFood,
+    removeMeal,
+    formIsValid,
+    isCreatingFavoriteMeal,
+    mealFoods,
+    foods,
+    control,
+    internalControl,
+    isFetchingFoods,
+    foodOptions,
+    measureOptions,
+  } = useModalCreateFavoriteMealHook(props);
+
+  return (
+    <Modal.Root
+      isOpen={isOpen}
+      onClose={handleCloseModal}
+      className="max-h-full overflow-y-auto"
+    >
+      <Modal.Header>
+        <Modal.Title>Criar uma nova refeição favorita</Modal.Title>
+
+        <Modal.Description>
+          Preencha os dados abaixo para criar uma nova refeiçao favorita.
+        </Modal.Description>
+      </Modal.Header>
+      <form
+        id="modal-create-favorite-meal"
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-2"
+      >
+        <Controller
+          control={control}
+          name="name"
+          render={({ field: { value, onChange } }) => (
+            <Input
+              name="name"
+              placeholder="Nome da refeição"
+              value={value}
+              onChange={onChange}
+            />
+          )}
+        />
+
+        <div>
+          <Controller
+            control={internalControl}
+            name="foodId"
+            render={({ field: { value, onChange } }) => (
+              <SelectAutoComplete
+                isLoading={isFetchingFoods}
+                placeholder="Selecione um alimento"
+                options={foodOptions}
+                value={value}
+                onChange={onChange}
+              />
+            )}
+          />
+        </div>
+
+        <Controller
+          control={internalControl}
+          name="measure"
+          render={({ field: { value, onChange } }) => (
+            <Select value={value} onValueChange={onChange}>
+              <SelectTrigger className="h-8  data-[placeholder]:text-muted-foreground sm:h-[3.25rem] ">
+                <SelectValue placeholder="Selecione a unidade de medida" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  {measureOptions.map((measure, index) => (
+                    <SelectItem
+                      value={measure.name}
+                      key={`measure-${index}-${measure.name}`}
+                    >
+                      {measure.name}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          )}
+        />
+
+        <Controller
+          control={internalControl}
+          name="qty"
+          render={({ field: { value, onChange } }) => (
+            <SimpleInput
+              value={parseFloat(value.toString())}
+              onChange={(event) => {
+                // const newValue = event.target.value.replace(/^0+/, '');
+                // const numberValue = parseFloat(newValue);
+
+                onChange(event.target.valueAsNumber);
+              }}
+              placeholder="Adicione a quantidade"
+            />
+          )}
+        />
+      </form>
+
+      <Button onClick={handleAppendMealFood} disabled={isCreatingFavoriteMeal}>
+        Adicionar
+      </Button>
+
+      <Separator />
+
+      {mealFoods.map((mealFood, index) => {
+        const selectedFood = foods.find((food) => food.id === mealFood.foodId);
+
+        if (!selectedFood) return;
+
+        return (
+          <div
+            key={`meal-food-${mealFood.id}`}
+            className="flex items-center justify-between"
+          >
+            <div className="flex flex-col ">
+              <small>{selectedFood.name}</small>
+              <small>
+                {mealFood.qty} - {mealFood.measure}
+              </small>
+            </div>
+
+            <div>
+              <Button
+                variant={'destructive'}
+                className="h-6 px-1"
+                onClick={() => removeMeal(index)}
+              >
+                <TrashIcon />
+              </Button>
+            </div>
+          </div>
+        );
+      })}
+
+      <Modal.Footer>
+        <Button
+          onClick={handleCloseModal}
+          variant={'destructive'}
+          disabled={isCreatingFavoriteMeal}
+        >
+          Cancelar
+        </Button>
+        <Button
+          disabled={!formIsValid}
+          type="submit"
+          form="modal-create-favorite-meal"
+          isLoading={isCreatingFavoriteMeal}
+        >
+          Criar
+        </Button>
+      </Modal.Footer>
+    </Modal.Root>
+  );
+}
