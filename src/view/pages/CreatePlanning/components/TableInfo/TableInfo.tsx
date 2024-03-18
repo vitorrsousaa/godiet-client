9f6f96d2c351363195bcb49fc8bc6d32cea30f3c
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-18 15:46:00 -0300

    refactor: changes table info for each component

--- src/view/pages/CreatePlanning/components/TableInfo/TableInfo.tsx
+++ src/view/pages/CreatePlanning/components/TableInfo/TableInfo.tsx
@@ -7,72 +7,92 @@ import {
   TableHeader,
   TableRow,
 } from '@godiet-ui/Table';
+import { cn } from '@godiet-utils/cn';
 
 import { Pencil1Icon, TrashIcon } from '@radix-ui/react-icons';
-
-import { useTableInfoHook } from './TableInfo.hook';
 
 interface openModalEditParams {
   mealFoodIndex: number;
   mealIndex: number;
 }
 
+interface FoodsByMeal {
+  id: string;
+  measure: { name: string; qty: number };
+  qty: number;
+  prot: string;
+  fat: string;
+  carb: string;
+  energy: string;
+  name: string;
+}
+
 export interface TableInfoProps {
   mealIndex: number;
-  onOpenModalRemove: (index: number) => void;
-  onOpenModalEdit: (params: openModalEditParams) => void;
+  onOpenModalRemove?: (index: number) => void;
+  onOpenModalEdit?: (params: openModalEditParams) => void;
+  disabledActions?: boolean;
+  mealFoods: FoodsByMeal[];
 }
 
 export function TableInfo(props: TableInfoProps) {
-  const { mealIndex, onOpenModalRemove, onOpenModalEdit } = props;
-
-  const { foodsByMeal } = useTableInfoHook(props);
+  const {
+    mealIndex,
+    mealFoods,
+    disabledActions = false,
+    onOpenModalRemove,
+    onOpenModalEdit,
+  } = props;
 
   return (
     <>
-      {foodsByMeal.length > 0 ? (
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="">Alimento</TableHead>
-              <TableHead className="hidden min-[430px]:table-cell">
-                Quantidade
-              </TableHead>
-              <TableHead className="hidden sm:table-cell">Prot</TableHead>
-              <TableHead className="hidden sm:table-cell">Carb</TableHead>
-              <TableHead className="hidden sm:table-cell">Gord</TableHead>
-              <TableHead>Cal</TableHead>
-              <TableHead className="flex  items-center justify-center">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="">Alimento</TableHead>
+            <TableHead className="hidden min-[430px]:table-cell">
+              Quantidade
+            </TableHead>
+            <TableHead className="hidden sm:table-cell">Prot</TableHead>
+            <TableHead className="hidden sm:table-cell">Carb</TableHead>
+            <TableHead className="hidden sm:table-cell">Gord</TableHead>
+            <TableHead>Cal</TableHead>
+            {!disabledActions && (
+              <TableHead className={cn('flex items-center justify-center')}>
                 Ações
               </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {foodsByMeal.map((foods, index) => {
-              const { carb, energy, fat, id, name, prot, qty, measure } = foods;
+            )}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {mealFoods.map((foods, index) => {
+            const { carb, energy, fat, id, name, prot, qty, measure } = foods;
 
-              return (
-                <TableRow key={`mealfood-${id}-${index}`}>
-                  <TableCell className="text-[12px]">{name}</TableCell>
-                  <TableCell className="hidden text-[12px] min-[430px]:table-cell">
-                    {qty} {measure.name}
-                  </TableCell>
-                  <TableCell className="hidden text-[12px] sm:table-cell">
-                    {prot} (g)
-                  </TableCell>
-                  <TableCell className="hidden text-[12px] sm:table-cell">
-                    {carb} (g)
-                  </TableCell>
-                  <TableCell className="hidden text-[12px] sm:table-cell">
-                    {fat} (g)
-                  </TableCell>
-                  <TableCell className="text-[12px]">{energy} Kcal</TableCell>
+            return (
+              <TableRow key={`mealfood-${id}-${index}`}>
+                <TableCell className="text-[12px]">{name}</TableCell>
+                <TableCell className="hidden text-[12px] min-[430px]:table-cell">
+                  {qty} {measure.name}
+                </TableCell>
+                <TableCell className="hidden text-[12px] sm:table-cell">
+                  {prot} (g)
+                </TableCell>
+                <TableCell className="hidden text-[12px] sm:table-cell">
+                  {carb} (g)
+                </TableCell>
+                <TableCell className="hidden text-[12px] sm:table-cell">
+                  {fat} (g)
+                </TableCell>
+                <TableCell className="text-[12px]">{energy} Kcal</TableCell>
+                {!disabledActions && (
                   <TableCell>
                     <span className="flex w-full flex-row items-center justify-center gap-1">
                       <Button
                         variant={'outline'}
                         className="h-6 px-1 transition-colors hover:bg-red-400 "
-                        onClick={() => onOpenModalRemove(index)}
+                        onClick={() =>
+                          onOpenModalRemove && onOpenModalRemove(index)
+                        }
                       >
                         <TrashIcon />
                       </Button>
@@ -80,28 +100,23 @@ export function TableInfo(props: TableInfoProps) {
                         variant={'outline'}
                         className="h-6 px-1 transition-colors hover:bg-gray-300 "
                         onClick={() => {
-                          onOpenModalEdit({
-                            mealFoodIndex: index,
-                            mealIndex,
-                          });
+                          onOpenModalEdit &&
+                            onOpenModalEdit({
+                              mealFoodIndex: index,
+                              mealIndex,
+                            });
                         }}
                       >
                         <Pencil1Icon />
                       </Button>
                     </span>
                   </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      ) : (
-        <div>
-          <p className="text-sm">
-            Você não adicionou alimentos para esta refeição.
-          </p>
-        </div>
-      )}
+                )}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
     </>
   );
 }
