919da98af14858fc6aa2d9d710326af9e69461c9
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-18 16:26:46 -0300

    fix: fixes importation of entitie

--- src/app/entities/index.ts
+++ src/app/entities/index.ts
@@ -1,6 +1,6 @@
 export type { User } from './User';
 export type { TPatient } from './Patient';
-export type { TFood } from './Food';
+export * from './Food';
 export type { TCategoryName } from './CategoryName';
 export type { TAnamnesisTemplate } from './TAnamnesisTemplate';
 export * from './planningMeal';
