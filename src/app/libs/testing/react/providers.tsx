fe5f6685a8380fa8615dfe7511432936d16f8ee1
Vitor Sousa <v.sousa.cf@gmail.com>
2024-03-25 03:33:14 -0300

    test(GODIET-68): :white_check_mark: update providers of tests with react hook form

--- src/app/libs/testing/react/providers.tsx
+++ src/app/libs/testing/react/providers.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useState } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { FormProvider, useForm } from 'react-hook-form';
 import { BrowserRouter } from 'react-router-dom';
 
 type ProvidersProps = {
@@ -12,9 +13,15 @@ function AllTheProviders(props: ProvidersProps) {
 
   const [queryClient] = useState(() => new QueryClient());
 
+  const defaultValues = {};
+
+  const methods = useForm({ defaultValues });
+
   return (
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <FormProvider {...methods}>{children}</FormProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
