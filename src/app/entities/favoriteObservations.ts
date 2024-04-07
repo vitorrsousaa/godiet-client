956f5f5854b79b048e841f1c59bbef7d0775f26d
Vitor Sousa <v.sousa.cf@gmail.com>
2024-04-07 16:57:55 -0300

    chore(GODIET-74): :label: create types to favorite observation

---
+++ src/app/entities/favoriteObservations.ts
@@ -0,0 +1,11 @@
+export interface TFavoritesObservation {
+  createdAt: string;
+
+  id: string;
+
+  text: string;
+
+  title: string;
+
+  userId: string;
+}
--- src/app/services/favoritesObservation/getAll.ts
+++ src/app/services/favoritesObservation/getAll.ts
@@ -1,7 +1,10 @@
+import { TFavoritesObservation } from '@godiet-entities';
 import { httpClient } from '@godiet-services/httpClient';
 
 export async function getAll() {
-  const { data } = await httpClient.get('/observation-template');
+  const { data } = await httpClient.get<TFavoritesObservation[]>(
+    '/observation-template'
+  );
 
   return data;
 }
