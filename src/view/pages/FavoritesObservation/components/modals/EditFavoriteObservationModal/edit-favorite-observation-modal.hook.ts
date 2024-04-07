bb72561fe51cb8c863e4bf5105d01c98c4cae434
Vitor Sousa <v.sousa.cf@gmail.com>
2024-04-07 18:03:27 -0300

    feat(GODIET-74): :triangular_flag_on_post: initialize edit favorite observation modal feature

---
+++ src/view/pages/FavoritesObservation/components/modals/EditFavoriteObservationModal/edit-favorite-observation-modal.hook.ts
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { useUpdateFavoritesObservation } from '@godiet-hooks/favoritesObservation';
+
+import toast from 'react-hot-toast';
+
+import {
+  TCreateFavoriteObservationDTO,
+  useFavoriteObservationFormController,
+} from '../../FavoriteObservationForm';
+
+import { EditFavoriteObservationModalProps } from './edit-favorite-observation-modal';
+
+export function useEditFavoriteObservationModalHook(
+  props: EditFavoriteObservationModalProps
+) {
+  const { initialValues, onClose } = props;
+
+  const controller = useFavoriteObservationFormController();
+
+  const { isUpdatingFavoritesObservation, updateFavoritesObservation } =
+    useUpdateFavoritesObservation();
+
+  const handleCloseModal = React.useCallback(() => {
+    controller.reset();
+    onClose();
+  }, [controller, onClose]);
+
+  const handleSubmit = React.useCallback(
+    async (data: TCreateFavoriteObservationDTO) => {
+      try {
+        await updateFavoritesObservation({
+          id: initialValues.id,
+          text: data.text,
+          title: data.title,
+        });
+
+        toast.success('Observação editada com sucesso');
+      } catch {
+        toast.error('Erro ao editar observação favorita');
+      } finally {
+        handleCloseModal();
+      }
+    },
+    [handleCloseModal, initialValues.id, updateFavoritesObservation]
+  );
+
+  return {
+    controller,
+    isUpdatingFavoritesObservation,
+    handleCloseModal,
+    handleSubmit,
+  };
+}
---
+++ src/view/pages/FavoritesObservation/components/modals/EditFavoriteObservationModal/index.ts
@@ -0,0 +1,7 @@
+import {
+  EditFavoriteObservationModal,
+  EditFavoriteObservationModalProps,
+} from './edit-favorite-observation-modal';
+
+export { EditFavoriteObservationModal };
+export type { EditFavoriteObservationModalProps };
