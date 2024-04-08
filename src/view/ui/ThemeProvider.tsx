b228c1731eaddcec5f1cdd7f6849ee8ce32ffd00
Vitor Sousa <v.sousa.cf@gmai.com>
2024-04-08 15:08:14 -0300

    update test theme provider

--- src/view/ui/ThemeProvider.tsx
+++ src/view/ui/ThemeProvider.tsx
@@ -21,7 +21,6 @@ export function ThemeProvider({
 
   useEffect(() => {
     const root = window.document.documentElement;
-
     root.classList.remove('light', 'dark');
 
     if (theme === 'system') {
